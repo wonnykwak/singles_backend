95d320b0970de59ab4f1dc44257d876886918641
FelixMo42 <felix@tacocat.com>
2019-07-10 10:42:59 -0700

    updated db format

--- webserver2.py
+++ webserver2.py
@@ -1,89 +1,74 @@
 import sqlite3
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, g
+from contextlib import closing
 
 app = Flask(__name__, static_folder='public')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
 
-def create_connection(db_file):
-    try:
-        conn = sqlite3.connect(db_file)
-        return conn
-    except Exception as e:
-        print(e)
- 
-    return None
-    
-def create_table(conn, create_table_sql, add_values_sql):
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-        c.execute(add_values_sql)
-        array = c.execute("SELECT id, name, name_of_advice, name_of_move, used_advice from projects")
-        #array2 = c.execute("SELECT id, text, priority, status_id, project_id, begin_date, end_date,  from actions")
-        for row in array:
-            print ("ID = ", row[0])
-            print ("NAME = ", row[1])
-            print ("ADVICE = ", row[2])
-            print ("USER MOVE = ", row[3])
-            print ("DID USER USE ADVICE = ", row[4], "\n")
+DATABASE = "database.db"
 
-        #for row2 in array2:
-         #   print ("ID = ", row2[0])
-          #  print ("NAME = ", row2[1])
-           # print ("PRIORITY  = ", row2[2])
-            #print ("STATUS_ID = ", row2[3], "\n")
-            #print ("PROJECT_ID = ", row2[0])
-            #print ("BEGIN_DATE = ", row2[1])
-            #print ("END_DATE = ", row2[2])
-        
-    except Exception as e:
-        print(create_table_sql)
-        print(e)
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
+def get_cur():
+    return closing(get_db().cursor())
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 def main():
-    database = "database.db"
-    
-    sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS projects (
-                                        id integer PRIMARY KEY,
-                                        name text NOT NULL,
-                                        name_of_advice text,
-                                        name_of_move text,
-                                        used_advice boolean
-                                    );"""
-    sql_add_projects = ''' INSERT INTO projects (id, name, name_of_advice, name_of_move, used_advice)
-                           VALUES (21, 'Eduardo', 'dont put it next to the 2', 'next to 2', 'False');'''
- 
-    #sql_create_actions_table = """CREATE TABLE IF NOT EXISTS actions (
-                                    #id integer PRIMARY KEY,
-                                    #name text NOT NULL,
-                                    #priority integer,
-                                    #status_id integer NOT NULL,
-                                    #project_id integer NOT NULL,
-                                    #begin_date text NOT NULL,
-                                    #end_date text NOT NULL
-                                #);"""
+    sql_delete_moves_table = """ DROP TABLE IF EXISTS moves """
+    sql_create_moves_table = """ CREATE TABLE IF NOT EXISTS moves (
+                                    id INTEGER NOT NULL,
+                                    move INTEGER NOT NULL,
+                                    type TEXT NOT NULL,
+                                    advice TEXT,
+                                    PRIMARY KEY (id, move)
+                                ); """
 
-    #sql_add_actions = ''' INSERT INTO actions (id, name, priority, status_id, project_id, begin_date, end_date)
-                       # VALUES (22, 'bob', 1, 3, 5, 'january', 'december');'''
+    sql_add_move = ''' INSERT INTO moves (id, move, type, advice)
+                           VALUES (21, 21, 'dont put it next to the 2', 'next to 2');'''
 
-    conn = create_connection(database)
-    if conn is not None:
-        create_table(conn, sql_create_projects_table, sql_add_projects)
-       # create_table(conn, sql_create_actions_table, sql_add_actions)
+    with app.app_context():
+        with get_cur() as c:
+            c.execute(sql_delete_moves_table)
+            c.execute(sql_create_moves_table)
 
-    else:
-        print("Error! cannot create the database connection.")
-    
-@app.route("/home")
-def bla():
-    return "Welcome to my world"
+@app.route("/api/saveMove")
+def saveMove():
+    with get_cur() as c:
+        data = {
+            "id": 0,
+            "move": 0,
+            "type": "good",
+            "advice": "bad"
+        }
+
+        c.execute( "INSERT INTO moves (id, move, type, advice) VALUES ({id}, {move}, '{type}', '{advice}')".format(**data) )
+
+        get_db().commit()
+
+        return "", 200
 
 
+@app.route("/api/readMove")
+def readMove():
+    with get_cur() as c:
+        array = c.execute("SELECT * FROM moves").fetchall()
+        for row in array:
+            print(row)
+
+        return "Welcome to my world"
 
 if __name__ == '__main__':
-    main()
-    app.run(debug=True, host = "0.0.0.0", port= 4000) 
-
+    try:
+        main()
+    except Exception as e:
+        print(e)
+    
+    app.run(debug=True, host="0.0.0.0", port=4000)
